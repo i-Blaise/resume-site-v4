32860869a61389ea99c29e022c1c65b89686878f
i-Blaise <menniablaise@yahoo.com>
2025-07-25 08:56:29 +0000

    Refactor WorkImages component layout for improved responsiveness and consistency

--- src/Components/WorkComponents/WorkImages_3.jsx
+++ src/Components/WorkComponents/WorkImages_3.jsx
@@ -10,19 +10,19 @@ export default function WorkImages({ images = []}) {
                 className="relative h-[550px] w-auto lg:w-auto md:h-[554px] bg-cover bg-center bg-no-repeat rounded-[13px] mt-6"
             style={{ backgroundImage: `url(${images[0]})` }}>
             </div>
-            <div 
+            <div className="flex flex-col lg:flex-row md:flex-row gap-6 justify-between mt-6 w-full border border-blue-500">
+                <div 
                 data-aos="fade-in"
                 data-aos-delay="200"         // ms delay
                 data-aos-duration="800"
-                className="flex flex-col lg:flex-row md:flex-row gap-6 justify-between items-center mt-6">
-                <div className="relative h-[518px] w-[320px] lg:w-[520px] md:w-[520px] md:h-[520px] bg-cover bg-center bg-no-repeat rounded-[13px]"
+                className="relative h-[518px] w-full lg:w-[50%] md:w-[50%] md:h-[520px] bg-cover bg-center bg-no-repeat rounded-[13px]"
                 style={{ backgroundImage: `url(${images[1]})` }}>
                 </div>
                 <div 
                 data-aos="fade-in"
                 data-aos-delay="300"         // ms delay
                 data-aos-duration="800"
-                className="relative h-[518px] w-[320px] lg:w-[828px] md:h-[520px] bg-cover bg-center bg-no-repeat rounded-[13px]"
+                className="relative h-[518px] w-full lg:w-[50%] md:w-[50%] md:h-[520px] bg-cover bg-center bg-no-repeat rounded-[13px]"
                 style={{ backgroundImage: `url(${images[2]})` }}>
                 </div>
             </div>
